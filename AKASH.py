497a06a7c751f4c6975badf79819361e26df4aaf
HADI ANHAF AIMAN <122558001+AKASH-CYBER-143@users.noreply.github.com>
2023-01-17 10:29:18 +0600

    Update AKASH.py

--- AKASH.py
+++ AKASH.py
@@ -1,1 +1,23 @@
-import XXD
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import XDD
+
+elif bit == '32bit':
+
+    import XDD
