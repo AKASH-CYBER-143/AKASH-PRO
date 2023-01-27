19480e567b02445345693fd6d44c75f9a4e50978
HADI ANHAF AIMAN <122558001+AKASH-CYBER-143@users.noreply.github.com>
2023-01-27 23:12:05 +0600

    Update AKASH.py

--- AKASH.py
+++ AKASH.py
@@ -1,1 +1,24 @@
-import PAID
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import PAID
+
+elif bit == '32bit':
+
+    import PAID
+
